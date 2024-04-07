d728aef4ce3a5592ce153de245a16dfab487abbc
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-04-07 22:16:26 +0530

    express.json()

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const db = require('./models/dbconnection')
-
+const router = require('./routes/routes')
 db()
-
+app.use(express.json())
 const PORT = 3005 || 5001
 
-
+app.use('/api',router)
 
 app.listen(PORT,()=>console.log(`Server runnin at ${PORT}`))
